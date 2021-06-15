e87792a80d89f927015b3d9e15274b0bd8bc6fe0
NovikovPavel-vki <Dotanam@mail.ru>
2021-06-16 05:04:36 +0700

    CMF

---
+++ CMF/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace CMF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(352, 369);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 69);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Translate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 146);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBox2.Location = new System.Drawing.Point(544, 26);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(264, 146);
+            this.textBox2.TabIndex = 10;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Rus",
+            "Eng"});
+            this.checkedListBox1.Location = new System.Drawing.Point(37, 246);
+            this.checkedListBox1.MultiColumn = true;
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(264, 58);
+            this.checkedListBox1.TabIndex = 11;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "Code",
+            "Letter"});
+            this.checkedListBox2.Location = new System.Drawing.Point(544, 246);
+            this.checkedListBox2.MultiColumn = true;
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(264, 58);
+            this.checkedListBox2.TabIndex = 12;
+            this.checkedListBox2.SelectedIndexChanged += new System.EventHandler(this.checkedListBox2_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(530, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Transport";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(530, 369);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(544, 197);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Copy";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(37, 197);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Paste";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(906, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ CMF/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibMorseCode;
+
+namespace CMF
+{
+    public partial class Form1 : Form
+    {
+        string language;
+        string typeC;
+        string textData;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                        string stringCode = null, //Хранит шифр
+               resultString = ""; //Хранит результат
+           // char bufString = '\0'; //строка буфер для посимвольного считывания шифра из файла
+            bool isMessageExist = false; //Проверка на то, есть ли сообщение в исходном файле
+            int countProbel = 0; //Количество пробелов
+            textBox2.Clear();
+            string bufString = textBox1.Text;
+
+            MorseCode message = new MorseCode(language);
+
+            int index = 0;
+            if (typeC == "c".ToString()) 
+            {
+                while (index < bufString.Length)
+                {
+                    while (bufString[index] != ' ') //Считываем символы, пока не получим шифр
+                    {
+                        
+
+                        if (bufString[index] == '\uffff') //Конец файла сообщения
+                            break;
+
+                        //Ведем подсчет пробелов для разделения слов в сообщении
+                        if (bufString[index] != ' ')
+                            countProbel = 0;
+                      
+
+                        //Запись кода
+                        if (bufString[index] != ' ')
+                            stringCode += bufString[index];
+
+                        index++;
+                        if (index >= bufString.Length)
+                            break;
+                    }
+
+                    //Дешифрация кода
+                    if (stringCode != null)
+                        resultString += Convert.ToString(message.Decode(stringCode));
+
+                    //Разделение слов между друг другом
+                    if (countProbel == 2)
+                        resultString += Convert.ToString(message.Decode(Convert.ToString(bufString[index])));
+
+                    stringCode = null;
+
+                    countProbel++;
+
+                    index++;
+                }
+
+                
+
+            }
+            else
+            {
+                while (index < bufString.Length)
+                {
+                    //шифрование символа из сообщения
+                    resultString += message.Code(bufString[index]);
+                    index++;
+                }
+                
+            }
+                
+
+
+            textBox2.Text = resultString.ToString();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count > 0)
+            {
+                for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                    checkedListBox1.SetItemChecked(i, false);
+                checkedListBox1.SetItemChecked(checkedListBox1.SelectedIndex, true);
+            }
+            if (checkedListBox1.GetItemCheckState(0) == CheckState.Checked)
+                language = "rus";
+            if (checkedListBox1.GetItemCheckState(1) == CheckState.Checked)
+                language = "en";
+            
+        }
+
+        private void checkedListBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (checkedListBox2.CheckedItems.Count > 0)
+            {
+                for (int i = 0; i < checkedListBox2.Items.Count; i++)
+                    checkedListBox2.SetItemChecked(i, false);
+                checkedListBox2.SetItemChecked(checkedListBox2.SelectedIndex, true);
+            }
+
+            if (checkedListBox2.GetItemCheckState(0) == CheckState.Checked)
+                typeC = "c";
+            if (checkedListBox2.GetItemCheckState(1) == CheckState.Checked)
+                typeC = "l";
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text;
+            textBox2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+             textData = textBox2.Text;
+            Clipboard.SetData(DataFormats.Text, (Object)textData);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textData.ToString();
+            Clipboard.SetData(DataFormats.Text, (Object)textData);
+        }
+    }
+    
+      
+    }
+
+
+
+
